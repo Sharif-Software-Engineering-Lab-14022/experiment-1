ab560d00c6a9555b765e063b241e0bec746a9087
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-03-07 18:40:40 +0330

    feat: add home page

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import { MainLayout } from "../../Layouts"
+
+function HomePage() {
+  return <MainLayout>
+    <h1 className="text-6xl font-bold p-4 h-[100vh]">Calculator</h1>
+  </MainLayout>
+}
+
+export default HomePage
