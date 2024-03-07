7f7e0ba6f7ab8e803dada0a1461e09fe01b89ad6
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-03-07 18:57:09 +0330

    chore: copyright

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,6 +5,12 @@ function HomePage() {
   return <MainLayout>
     <h1 className="text-6xl font-bold p-4">Calculator</h1>
     <Calculator />
+    <div className="flex items-end justify-center text-xs font-light p-2">
+      <span className="mr-2">Copyright © 2024 - SE Lab.</span>
+      <a href="https://github.com/Sharif-Software-Engineering-Lab-14022/experiment-1" target="_blank" rel="noreferrer">
+        <img className="w-[25px] h-[25px]" src="/github-logo.svg" alt="Github Logo"/>
+      </a>
+    </div>
   </MainLayout>
 }
 
