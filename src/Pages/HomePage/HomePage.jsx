fb6a04e354fae683fae9ee3a450a8894a41a1c39
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-03-09 00:55:44 +0330

    Change Github logo url

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,17 +1,19 @@
-import { MainLayout } from "../../Layouts"
+import {MainLayout} from "../../Layouts"
 import {Calculator} from "../../Templates";
+import GitHubLogo from "../../../public/github-logo.svg";
 
 function HomePage() {
-  return <MainLayout>
-    <h1 className="text-6xl font-bold p-4">SE Lab. Calculator</h1>
-    <Calculator />
-    <div className="flex items-center justify-center text-xs font-light p-2">
-      <span className="mr-2">Copyright © 2024 - SE Lab. - Amir Mohammad Fakhimi - Mohammad Amin Lotfi - Mana Peiravian</span>
-      <a href="https://github.com/Sharif-Software-Engineering-Lab-14022/experiment-1" target="_blank" rel="noreferrer">
-        <img className="w-[25px] h-[25px]" src="../../../public/github-logo.svg" alt="Github Logo"/>
-      </a>
-    </div>
-  </MainLayout>
+    return <MainLayout>
+        <h1 className="text-6xl font-bold p-4">SE Lab. Calculator</h1>
+        <Calculator/>
+        <div className="flex items-center justify-center text-xs font-light p-2">
+            <span className="mr-2">Copyright © 2024 - SE Lab. - Amir Mohammad Fakhimi - Mohammad Amin Lotfi - Mana Peiravian</span>
+            <a href="https://github.com/Sharif-Software-Engineering-Lab-14022/experiment-1" target="_blank"
+               rel="noreferrer">
+                <img className="w-[25px] h-[25px]" src={GitHubLogo} alt="GitHub Logo"/>
+            </a>
+        </div>
+    </MainLayout>
 }
 
 export default HomePage
