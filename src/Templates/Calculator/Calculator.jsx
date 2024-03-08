ffe47abd28ff540c6b9d0279edded3436f718819
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-03-08 15:15:19 +0330

    Add calculation logic

--- src/Templates/Calculator/Calculator.jsx
+++ src/Templates/Calculator/Calculator.jsx
@@ -3,11 +3,25 @@ import NumberButton from "../../Components/NumberButton";
 import OperationButton from "../../Components/OperationButton";
 import {useState} from "react";
 
-
 const Calculator = () => {
     const [currentOperand, setCurrentOperand] = useState(undefined)
     const [prevOperand, setPrevOperand] = useState(undefined)
     const [operation, setOperation] = useState(undefined)
+
+    function calculate() {
+        switch (operation) {
+            case '+':
+                return parseFloat(prevOperand) + parseFloat(currentOperand)
+            case '-':
+                return parseFloat(prevOperand) - parseFloat(currentOperand)
+            case '*':
+                return parseFloat(prevOperand) * parseFloat(currentOperand)
+            case '/':
+                return parseFloat(prevOperand) / parseFloat(currentOperand)
+            default:
+                return currentOperand
+        }
+    }
 
 
     return <div className="flex items-center justify-center w-full h-[calc(100vh-165px)]">
@@ -26,11 +40,21 @@ const Calculator = () => {
                 {['/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+'].map(el =>
                     typeof el === 'number'
                         ? <NumberButton key={el} number={el} currentOperandSetter={setCurrentOperand}/>
-                        : <OperationButton key={el} operation={el}/>
+                        : <OperationButton key={el} operation={el} currentOperand={currentOperand}
+                                           setPrevOperand={setPrevOperand} setCurrentOperand={setCurrentOperand}
+                                           setOperation={setOperation} calculate={calculate}/>
                 )}
                 <NumberButton customClasses={'col-span-2'} number={0} currentOperandSetter={setCurrentOperand}/>
                 <NumberButton number={'.'} currentOperandSetter={setCurrentOperand}/>
                 <button
+                    onClick={() => {
+                        if (currentOperand === undefined || prevOperand === undefined || operation === undefined)
+                            return
+
+                        setCurrentOperand(calculate())
+                        setPrevOperand(undefined)
+                        setOperation(undefined)
+                    }}
                     className="flex item-center justify-center bg-green-500 hover:bg-green-600 p-4 rounded-[10px] text-xl">
                     =
                 </button>
