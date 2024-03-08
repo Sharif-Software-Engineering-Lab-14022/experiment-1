bfc3a1a4578fce4b048ac6e709133485b753db9e
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-03-08 13:53:44 +0330

    Change title and footer

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,10 +3,10 @@ import {Calculator} from "../../Templates";
 
 function HomePage() {
   return <MainLayout>
-    <h1 className="text-6xl font-bold p-4">Calculator</h1>
+    <h1 className="text-6xl font-bold p-4">SE Lab. Calculator</h1>
     <Calculator />
-    <div className="flex items-end justify-center text-xs font-light p-2">
-      <span className="mr-2">Copyright © 2024 - SE Lab.</span>
+    <div className="flex items-center justify-center text-xs font-light p-2">
+      <span className="mr-2">Copyright © 2024 - SE Lab. - Amir Mohammad Fakhimi - Mohammad Amin Lotfi - Mana Peiravian</span>
       <a href="https://github.com/Sharif-Software-Engineering-Lab-14022/experiment-1" target="_blank" rel="noreferrer">
         <img className="w-[25px] h-[25px]" src="/github-logo.svg" alt="Github Logo"/>
       </a>
