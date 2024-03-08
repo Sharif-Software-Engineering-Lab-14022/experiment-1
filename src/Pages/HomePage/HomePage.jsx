65ecda90987edd8424b269e27fc1357aabf1a6e1
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-03-08 13:32:36 +0330

    fix: change main title and footer

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,10 +3,10 @@ import {Calculator} from "../../Templates";
 
 function HomePage() {
   return <MainLayout>
-    <h1 className="text-6xl font-bold p-4">Calculator</h1>
+    <h1 className="text-6xl font-bold p-4">Simple Calculator</h1>
     <Calculator />
     <div className="flex items-end justify-center text-xs font-light p-2">
-      <span className="mr-2">Copyright © 2024 - SE Lab.</span>
+      <span className="mr-2">Copyright © 2024 - Software Engineering Lab.</span>
       <a href="https://github.com/Sharif-Software-Engineering-Lab-14022/experiment-1" target="_blank" rel="noreferrer">
         <img className="w-[25px] h-[25px]" src="/github-logo.svg" alt="Github Logo"/>
       </a>
